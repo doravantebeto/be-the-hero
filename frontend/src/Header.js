1dde72eab01239448a01fc8b1f5afc2581c6c3f7
DoravanteBeto <41833666+doravantebeto@users.noreply.github.com>
2020-03-26 20:23:14 -0300

    Fron-end do app corrigido

---
+++ frontend/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header({ children }) {
+
+    return (
+
+        <header>
+
+            <h1>{children}</h1>
+
+        </header>
+
+    )
+
+}

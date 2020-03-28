387b3684668fca17cacac05f300ea866ba5c7f79
DoravanteBeto <41833666+doravantebeto@users.noreply.github.com>
2020-03-28 15:09:10 -0300

    Testes aplicados

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,37 @@
+const request = require('supertest')
+const app = require('../../src/app')
+const connection = require('../../src/database/connection')
+
+describe('ONG', () => {
+
+    beforeEach(async () => {
+
+        await connection.migrate.rollback()
+        await connection.migrate.latest()
+
+    })
+
+    afterAll(async () => {
+
+        await connection.destroy()
+
+    })
+
+    it('should be able to create a new ONG', async () => {
+
+        const response = await request(app).post('/ongs').send({
+	
+            name: "VISCONDE DE SABUGOSAA222",
+            email: "visconde.nervoso@gmail.com",
+            whatsapp: "1100000000",
+            city: "Sopaulo",
+            uf: "SP"
+            
+        })
+
+        expect(response.body).toHaveProperty('id')
+        expect(response.body.id).toHaveLength(8)
+
+    })
+
+})
